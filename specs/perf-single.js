355a7564578c032bf0c6f7801fcbb9fc787fad59
Markus Dulghier <markus@trigo.at>
2018-05-26 11:55:26 +0200

    improvement: refactor filter-resource completely to improve performance

---
+++ specs/perf-single.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const data = require('./data-singleobj');
+// const data = {};
+const {
+	lensPath, view, set, clone, startsWith, filter: ramdaFilter, pipe, reverse,
+} = require('ramda');
+
+const filterResource = require('../lib/hapi-acl-plugin/lib/filter-resource');
+const filter = { role: '*',
+	path: '*',
+	method: '*',
+	key: '*.id',
+	when: () => {},
+	value: null
+};
+
+
+filterResource(data, filter);
